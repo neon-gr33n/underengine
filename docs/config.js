8efb9ad7f5e2b341f061bd49f8ab9ec39ff10912
Neon-Gr33n <nopropsneeded13@gmail.com>
2025-12-16 11:25:59 -0500

    Delete docs directory

--- docs/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    description:"A modern UNDERTALE fangame engine",
-  name:"underengine",
-  favicon:"assets/docsIcon.png",
-  themeColor:"#FF0000",
-  latestVersion:"v7.0-beta-1",
-  otherVersions:null,
-};
