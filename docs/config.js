a24291fc75d92e21fc57578dcf158393eb43a56a
Neon-Gr33n <nopropsneeded13@gmail.com>
2025-12-16 10:56:40 -0500

    Update docs/config.js

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,8 @@
 const config = {
-    favicon:"assets/docsIcon.png",
+    description:"A modern UNDERTALE fangame engine",
+  name:"underengine",
+  favicon:"assets/docsIcon.png",
   themeColor:"#FF0000",
-  description:"A modern UNDERTALE fangame engine",
   latestVersion:"v7.0-beta-1",
   otherVersions:null,
-  name:"underengine",
 };
