eee0f6e8da4f490df8372125382dd63b878a388e
Neon-Gr33n <nopropsneeded13@gmail.com>
2025-12-15 23:10:54 -0500

    Delete docs/config.js

--- docs/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    description:"Documentation for underengine",
-  name:"underengine",
-  favicon:"assets/docsIcon.png",
-  themeColor:"#FF0000",
-  latestVersion:"v7.0-beta-1",
-  otherVersions:null,
-};
