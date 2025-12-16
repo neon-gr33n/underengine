122757f75e130412531c4d684dc782225d655fd1
Neon-Gr33n <nopropsneeded13@gmail.com>
2025-12-15 19:32:47 -0500

    Update config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    description:"Documentation for Your Project",
+  name:"underengine",
+  favicon:"assets/docsIcon.png",
+  themeColor:"#FF0000",
+  latestVersion:"v6.0",
+  otherVersions:null,
+};
